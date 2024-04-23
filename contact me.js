0f022df3e2acd8a5f938c28330ea25e944b92608
S.muneiwa <128157791+moneiwa@users.noreply.github.com>
2024-04-23 14:18:23 +0200

    Add files via upload

---
+++ contact me.js
@@ -0,0 +1,5 @@
+function handleformSubmit (event) {
+   event.preventDefault()
+    console.log(event);
+}
+
---
+++ index.js
@@ -0,0 +1,20 @@
+// Personal Information
+var firstName = "Muneiwa";
+var lastName = "Sithagu";
+var bio = "Passionate graduate and tech enthusiast.";
+var contactEmail = "mhsithagu@gmail.com";
+var phoneNumber = "0636785999";
+
+// Educational Background
+var university = "Rosebank college";
+var degree = "Bachelor of information technology in Business system";
+var graduationYear = 2022
+
+// Displaying Information
+console.log("Name: " + firstName + " " + lastName);
+console.log("Bio: " + bio);
+console.log("Contact Email: " + contactEmail);
+console.log("Phone Number: " + phoneNumber);
+console.log("Education: " + degree + " from " + university + " (" + graduationYear + ")");
+
+alert ('Hello world')
